b3c48eaecf92a38d456fa6f65d1ed0c34011f170
nguyennhatduy <nguyennhatduy@vnpt.vn>
2023-08-12 22:47:23 +0700

    update readme

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 ```docker build -t tagname .```
 # Run
 ```docker run -p 5001:5001 tagname```
+```docker run -p 5001:5001 -e OPENAI_API_KEY=YOUR_OPENAI_API_KEY -e SERPER_API_KEY=YOUR_SERPER_API_KEY tagname```
--- apikey.py
+++ apikey.py
@@ -3,7 +3,7 @@ from typing import Tuple
 
 
 def load_env() -> Tuple[str, str]:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
     SERPER_API_KEY = os.getenv("SERPER_API_KEY", "e7ce86460e7210bef65bf76c1b4432800c9152dc")
 
     #OPENAI_API_KEY = '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'
